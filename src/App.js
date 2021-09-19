fe4e8e5f43ffccd1e264ca5e3762452b5750f0a7
Marzan1234eng <mdmarzanhossain@gmail.com>
2021-09-19 11:17:25 +0600

    country details onClick eventhandler reduce component added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
 import Country from "./components/Country/Country";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
       fetch("https://restcountries.eu/rest/v2/all")
@@ -15,12 +17,23 @@ function App() {
           .catch(error => console.log(error))
   }, [])
 
+    const handleAddCountry = (country) => {
+        const newCart = [...cart, country];
+        setCart(newCart);
+    };
+
   return (
     <div style={{textAlign: "center"}}>
       <h1>Country Loaded: {countries.length}</h1>
+        <h5>Country Added: {cart.length}</h5>
+        <Cart cart={cart}
+        >
+
+        </Cart>
         {
             countries.map(country =>
                 <Country
+                    handleAddCountry={handleAddCountry}
                     country={country}
                     key={country.alpha3Code}
                 >
